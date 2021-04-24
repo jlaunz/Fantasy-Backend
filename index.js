08813694e217199e7e9431690f6e3ae57775a99c
jubilantfk <3455127@gmail.com>
2021-04-24 17:57:19 +1200

    Replace hardcode localhost

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(passport.initialize())
 app.use(cookieParser())
 
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     res.header('Access-Control-Allow-Headers', 'Content-Type')
     res.header('Access-Control-Allow-Credentials', 'true')
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,41 +1,55 @@
-const express = require("express")
-const passport = require("passport")
+const express = require('express')
+const passport = require('passport')
 const router = express.Router()
 const Host = require('../models/host')
 
-router.get("/spotify", passport.authenticate("spotify", {
-    scope: ["user-read-email","user-read-private", "streaming", "user-modify-playback-state"]}
-))
+router.get(
+    '/spotify',
+    passport.authenticate('spotify', {
+        scope: [
+            'user-read-email',
+            'user-read-private',
+            'streaming',
+            'user-modify-playback-state',
+        ],
+    })
+)
 
-router.get("/spotify/callback/", passport.authenticate("spotify", {session: false}),
-    (req, res)=>{
-
-        res.cookie("user", req.user.id);
-        res.redirect("http://localhost:3000")
+router.get(
+    '/spotify/callback/',
+    passport.authenticate('spotify', { session: false }),
+    (req, res) => {
+        res.cookie('user', req.user.id)
+        res.redirect(process.env.CLIENT_URL)
     }
 )
 
-router.get("/guest", async (req,res,next)=>{
-    try{
-        await Host.findOne({"party.id": req.query.id}).then( async partyExists =>{
-            if (partyExists){
-                let partyDetails = await Host.aggregate([
-                    { "$match": { "party.id": req.query.id} },
-                    { "$group": {_id: null,
-                        id: {"$first": "$party.id"},
-                        name: {"$first": "$name"},
-                        tracks: {"$push": "$party.tracks"}}
-                    },
-                    { "$project": {id: 1, tracks: 1 ,name: 1, _id: 0} }
-                ])
-                res.status(200).json(partyDetails)
-            }else{
-                res.status(404).json({message: "couldn't find party"})
+router.get('/guest', async (req, res, next) => {
+    try {
+        await Host.findOne({ 'party.id': req.query.id }).then(
+            async (partyExists) => {
+                if (partyExists) {
+                    let partyDetails = await Host.aggregate([
+                        { $match: { 'party.id': req.query.id } },
+                        {
+                            $group: {
+                                _id: null,
+                                id: { $first: '$party.id' },
+                                name: { $first: '$name' },
+                                tracks: { $push: '$party.tracks' },
+                            },
+                        },
+                        { $project: { id: 1, tracks: 1, name: 1, _id: 0 } },
+                    ])
+                    res.status(200).json(partyDetails)
+                } else {
+                    res.status(404).json({ message: "couldn't find party" })
+                }
             }
-        })
-    }catch(err){
-        res.status(500).json({message: err.message})
-        res.end();
+        )
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+        res.end()
     }
 })
 
